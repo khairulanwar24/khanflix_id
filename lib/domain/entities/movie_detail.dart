54a3db9680e330bb332a6da1c0646cc4fcc9e403
Muhammad Khairul Anwar <khairul240102@gmail.com>
2024-05-03 15:31:02 +0700

    entities actor

--- lib/domain/entities/movie_detail.dart
+++ lib/domain/entities/movie_detail.dart
@@ -16,12 +16,13 @@ class MovieDetail with _$MovieDetail {
   }) = _MovieDetail;
 
   factory MovieDetail.fromJSON(Map<String, dynamic> json) => MovieDetail(
-      id: json['id'],
-      title: json['title'],
-      overview: json['overview'],
-      runtime: json['runtime'],
-      voteAverage: json['voteAverage'].toDouble(),
-      genres: List<String>.from(json['genres'].map((e) => e['name'])),
-      posterPath: json['poster_path'],
-      backdropPath: json['backdrop_path']);
+        id: json['id'],
+        title: json['title'],
+        overview: json['overview'],
+        runtime: json['runtime'],
+        voteAverage: json['voteAverage'].toDouble(),
+        genres: List<String>.from(json['genres'].map((e) => e['name'])),
+        posterPath: json['poster_path'],
+        backdropPath: json['backdrop_path'],
+      );
 }
